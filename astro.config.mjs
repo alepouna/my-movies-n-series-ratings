2ae747e3f25166439e085ea17348c29db3909669
Aurora Takemi <gpg@aurorais.me>
2026-05-28 22:43:10 +0300

    add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 import { defineConfig } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   output: "static",
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
